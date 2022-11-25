42d35f63e0fdf15c94a57bf6f30334cecd723646
ccirion01 <cotycirion94@gmail.com>
2022-11-25 08:18:25 -0300

    [Shopping Cart] Add entities to DbContext

--- Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.DbContexts
 {
@@ -7,6 +8,8 @@ namespace Mango.Services.ShoppingCartAPI.DbContexts
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
-        //public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
     }
 }
