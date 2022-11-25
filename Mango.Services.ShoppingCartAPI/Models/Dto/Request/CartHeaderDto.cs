40e0133d5c45bf94271413d562dec7a15dd0a30c
ccirion01 <cotycirion94@gmail.com>
2022-11-25 08:17:05 -0300

    Add Shopping Cart entities

---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/Request/CartHeaderDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ShoppingCartAPI.Models.Dto.Request
+{
+    public class CartHeaderDto
+    {
+        public int CartHeaderId { get; set; }
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/Response/ResponseDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.Dto.Response
+{
+    public class ResponseDto<T>
+    {
+        public bool IsSuccess { get; set; } = true;
+        public T Result { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public List<string> ErrorMessages { get; set; }
+
+        public void AddErrorMessage(string message)
+        {
+            if (ErrorMessages == null)
+                ErrorMessages = new List<string>();
+
+            ErrorMessages.Add(message);
+        }
+    }
+}
