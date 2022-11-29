22b577c083c293c5618a3b746069987775f30f7e
ccirion01 <cotycirion94@gmail.com>
2022-11-29 09:24:11 -0300

    [UI] Consume ShoppingCartAPI from Details action method

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -7,20 +7,25 @@ using UI.Services.IServices;
 
 namespace UI.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly IProductService _productService;
+        private readonly ICartService _cartService;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(IProductService productService, ILogger<HomeController> logger)
+        public HomeController(
+            IProductService productService, 
+            ICartService cartService, 
+            ILogger<HomeController> logger)
         {
             _productService = productService;
+            _cartService = cartService;
             _logger = logger;
         }
 
         public async Task<IActionResult> Index()
         {
-            ResponseDto response = await _productService.GetAllAsync("");
+            ResponseDto response = await _productService.GetAllAsync(await GetTokenAsync());
             List<ProductDto> products = new();
 
             if (response?.IsSuccess ?? false)
@@ -32,13 +37,47 @@ namespace UI.Controllers
         [Authorize]
         public async Task<IActionResult> Details(int productId)
         {
-            ResponseDto response = await _productService.GetByIdAsync(productId, "");
+            //TODO: Fetch 'Count' value from the Cart API
+            ResponseDto response = await _productService.GetByIdAsync(productId, await GetTokenAsync());
             ProductDto model = new();
 
             if (response?.IsSuccess ?? false)
                 model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
 
             return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Details")]
+        [Authorize]
+        public async Task<IActionResult> DetailsPost(ProductDto productDto)
+        {
+            var token = await GetTokenAsync();
+            CartDto cartDto = new()
+            {
+                CartHeader = new()
+                {
+                    UserId = GetUserId()
+                },
+                CartDetails = new List<CartDetailDto>()
+                {
+                    new()
+                    {
+                        Count = productDto.Count,
+                        ProductId = productDto.ProductId                        
+                    }
+                }
+            };
+            var product = await _productService.GetByIdAsync(productDto.ProductId, token);            
+            if (product?.IsSuccess ?? false)
+                cartDto.CartDetails.FirstOrDefault().Product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(product.Result));
+
+            ResponseDto response = await _cartService.AddToCartAsync(cartDto, token);
+
+            if (response?.IsSuccess ?? false)
+                return RedirectToAction(nameof(Index));
+
+            return View(productDto);
         }
 
         public IActionResult Privacy()
@@ -63,4 +102,4 @@ namespace UI.Controllers
             return SignOut("Cookies", "oidc");
         }
     }
-}
+} 
