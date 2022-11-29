251f7763bdaf418f16268f02bff346dd2589ee3f
ccirion01 <cotycirion94@gmail.com>
2022-11-29 09:24:32 -0300

    [UI] Controllers common logic in BaseController

---
+++ UI/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class BaseController : Controller
+    {
+        public async Task<string> GetTokenAsync() => await HttpContext.GetTokenAsync("access_token");
+        public string GetUserId() => User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+    }
+}
