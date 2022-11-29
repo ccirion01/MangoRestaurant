58cbb63e2b72061c25f66fe4fb00ce261344a92d
ccirion01 <cotycirion94@gmail.com>
2022-11-29 09:22:09 -0300

    [UI] Add ShoppingCart service logic

---
+++ UI/Services/CartService.cs
@@ -0,0 +1,37 @@
+﻿using UI.Models;
+using UI.Services.IServices;
+
+namespace UI.Services
+{
+    public class CartService : BaseService, ICartService
+    {
+        internal override string Url => SD.ShoppingCartAPIBase + "api/cart";
+        public CartService(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+        }
+
+        public async Task<ResponseDto> GetByUserIdAsync(string userId, string token)
+        {
+            return await SendAsync(
+                CreateApiRequest(SD.ApiType.GET, token, $"{Url}/{userId}"));
+        }
+        
+        public async Task<ResponseDto> AddToCartAsync(CartDto cart, string token)
+        {
+            return await SendAsync(
+                CreateApiRequest(SD.ApiType.POST, token, data: cart));
+        }
+
+        public async Task<ResponseDto> UpdateCartAsync(CartDto cart, string token)
+        {
+            return await SendAsync(
+                CreateApiRequest(SD.ApiType.PUT, token, data: cart));
+        }
+
+        public async Task<ResponseDto> RemoveFromCartAsync(int cartDetailId, string token)
+        {
+            return await SendAsync(
+                CreateApiRequest(SD.ApiType.DELETE, token, $"{Url}/{cartDetailId}"));
+        }
+    }
+}
