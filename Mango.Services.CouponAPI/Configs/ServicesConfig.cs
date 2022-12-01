3538e48df3410ba699607368ceabfb9fbc0c3210
ccirion01 <cotycirion94@gmail.com>
2022-12-01 09:33:39 -0300

    [Coupon] Add coupon repository

---
+++ Mango.Services.CouponAPI/Configs/ServicesConfig.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Services.CouponAPI.Repositories;
+
+namespace Mango.Services.CouponAPI.Configs
+{
+    public static class ServicesConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICouponRepository, CouponRepository>();
+        }
+    }
+}
