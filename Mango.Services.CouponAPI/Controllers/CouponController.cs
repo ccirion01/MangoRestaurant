1a7a43b5948dbe9c59d8e0878d033279858ecba2
ccirion01 <cotycirion94@gmail.com>
2022-12-01 09:33:47 -0300

    [Coupon] Add coupon controller

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,36 @@
+﻿using Mango.Services.CouponAPI.Models.Dto;
+using Mango.Services.CouponAPI.Models.Dto.Response;
+using Mango.Services.CouponAPI.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/coupon")]
+    public class CouponController
+    {
+        private ICouponRepository _repository;
+        public CouponController(ICouponRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("{couponCode}")]
+        public async Task<ResponseDto<CouponDto>> Get(string couponCode)
+        {
+            ResponseDto<CouponDto> response = new();
+            try
+            {
+                response.Result = await _repository.GetByCode(couponCode);
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.AddExceptionMessage(ex);
+            }
+            return response;
+        }
+    }
+}
