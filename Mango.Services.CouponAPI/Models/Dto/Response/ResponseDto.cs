d75742001b90270b78c536e728dd56da4a58e526
ccirion01 <cotycirion94@gmail.com>
2022-12-01 09:32:32 -0300

    [Coupon] Add models

---
+++ Mango.Services.CouponAPI/Models/Dto/Response/ResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto.Response
+{
+    public class ResponseDto<T>
+    {
+        public bool IsSuccess { get; set; } = true;
+        public T Result { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public List<string> ErrorMessages { get; set; }
+
+        public void AddExceptionMessage(Exception ex)
+        {
+            ErrorMessages ??= new List<string>();
+            ErrorMessages.Add(ex.ToString());
+        }
+    }
+}
